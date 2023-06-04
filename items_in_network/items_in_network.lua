cb142236dd98d21c6a036555f8e56e03171d2811
Joris Schelfhout <joris@shelfwood.co>
2023-06-04 22:32:44 +0200

    convert to mineOS

--- items_in_network/items_in_network.lua
+++ items_in_network/items_in_network.lua
@@ -1,5 +1,6 @@
 -- load the necessary libraries
 local component = require("component")
+local GUI = require("GUI")
 
 -- get a reference to the ME interface
 local meInterfaceAddress = component.list("me_interface")()
@@ -17,7 +18,39 @@ local meInterface = component.proxy(meInterfaceAddress)
 local items = meInterface.getItemsInNetwork()
 table.sort(items, function(a, b) return a.size > b.size end)
 
--- display the items
-for i, item in ipairs(items) do
-    print(i .. ": " .. item.label .. ", Count: " .. item.size)
+-- initialize the application and a layout for our items
+local application = GUI.application()
+local layout = application:addChild(GUI.layout(1, 1, application.width, application.height, 1, 1))
+
+-- helper function to add a page of items to the layout
+local function addPage(page)
+    local start = (page - 1) * 10 + 1
+    local finish = math.min(start + 9, #items)
+    for i = start, finish do
+        layout:addChild(GUI.text(1, 1, 0xFFFFFF, items[i].label .. ": " .. items[i].size))
+    end
 end
+
+-- add the first page of items
+addPage(1)
+
+-- add navigation buttons
+local previousButton = application:addChild(GUI.button(1, application.height, 10, 1, 0xFFFFFF, 0x555555, 0x880000,
+    0xFFFFFF, "< Previous"))
+previousButton.onTouch = function()
+    layout:removeChildren()
+    addPage(math.max(1, currentPage - 1))
+    application:draw(true)
+end
+
+local nextButton = application:addChild(GUI.button(application.width - 9, application.height, 10, 1, 0xFFFFFF, 0x555555,
+    0x880000, 0xFFFFFF, "Next >"))
+nextButton.onTouch = function()
+    layout:removeChildren()
+    addPage(math.min(math.ceil(#items / 10), currentPage + 1))
+    application:draw(true)
+end
+
+-- start the application
+application:draw(true)
+application:start()
