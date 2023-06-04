5461165f1b06ae553a275cfd16e6d75158d20683
Joris Schelfhout <joris@shelfwood.co>
2023-06-04 20:35:26 +0200

    Add check for Me interface

--- items_in_network/items_in_network.lua
+++ items_in_network/items_in_network.lua
@@ -1,8 +1,20 @@
 -- load the necessary libraries
 local component = require("component")
 
--- get a reference to the AE2 interface
-local meInterface = component.proxy(component.list("me_interface")())
+-- load the necessary libraries
+local component = require("component")
+
+-- get a reference to the ME interface
+local meInterfaceAddress = component.list("me_interface")()
+
+-- check if the ME Interface is connected
+if not meInterfaceAddress then
+    print("ME Interface not detected. Please check the connection.")
+    return
+end
+
+-- if it is connected, create the proxy and continue with the script
+local meInterface = component.proxy(meInterfaceAddress)
 
 -- get the items in the network and sort them
 local items = meInterface.getItemsInNetwork()
