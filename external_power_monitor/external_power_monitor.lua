a30677437a4707c3c1ad192911a5178b7ffe875e
Joris Schelfhout <joris@shelfwood.co>
2023-06-04 21:36:22 +0200

    Use larger text

--- external_power_monitor/external_power_monitor.lua
+++ external_power_monitor/external_power_monitor.lua
@@ -2,7 +2,6 @@
 local component = require("component")
 local computer = require("computer")
 local term = require("term")
-local shell = require("shell")
 local gpu = component.gpu
 
 -- Get a table of all screens
@@ -26,15 +25,14 @@ gpu.bind(originalScreen) -- Reset the binding to the original screen
 print("Enter the number of the screen you want to use:")
 local screenNumber = tonumber(io.read())
 
--- Get the screen address and create a proxy
-local screen = component.proxy(screenAddresses[screenNumber])
-
--- Get primary GPU and bind it to the selected screen
-local gpu = component.gpu
+-- Get the screen address and bind the GPU to it
 gpu.bind(screenAddresses[screenNumber])
 
 -- Get screen resolution
 local w, h = gpu.getResolution()
+
+-- Set font size
+gpu.setResolution(w / 6, h / 6)
 
 -- Initialize cursor position
 local cursorX, cursorY = 1, 1
@@ -51,17 +49,19 @@ end
 
 -- Update the energy status every 0.5 seconds
 while true do
-    -- Get and print the current energy
-    local currentEnergy = computer.energy()
-    printScreen("Current Energy: " .. currentEnergy)
+    -- Get and print the current energy in RF and EU
+    local currentEnergyRF = computer.energy()
+    local currentEnergyEU = currentEnergyRF / 4 -- Convert to EU
+    printScreen("Current Energy: " .. currentEnergyRF .. " RF / " .. string.format("%.2f", currentEnergyEU) .. " EU")
 
-    -- Get and print the maximum energy
-    local maxEnergy = computer.maxEnergy()
-    printScreen("Max Energy: " .. maxEnergy)
+    -- Get and print the maximum energy in RF and EU
+    local maxEnergyRF = computer.maxEnergy()
+    local maxEnergyEU = maxEnergyRF / 4 -- Convert to EU
+    printScreen("Max Energy: " .. maxEnergyRF .. " RF / " .. string.format("%.2f", maxEnergyEU) .. " EU")
 
     -- Calculate and print the percentage of energy remaining
-    local energyPercent = (currentEnergy / maxEnergy) * 100
-    printScreen("Energy Remaining: " .. energyPercent .. "%")
+    local energyPercent = (currentEnergyRF / maxEnergyRF) * 100
+    printScreen("Energy Remaining: " .. string.format("%.2f", energyPercent) .. "%")
 
     -- Sleep for 0.5 seconds before the next update
     os.sleep(0.5)
